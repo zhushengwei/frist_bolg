16349f8bc566ab5b26cfea4ec97d70a894801164
zhushengwei <15918660832@139.com>
2016-07-27 08:48:11 +0800

    test_four

--- app/models.py
+++ app/models.py
@@ -209,7 +209,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
-    # body_html = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -230,10 +230,11 @@ class Post(db.Model):
 
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
-        allowed_tags = ['a', 'abbr','acronym', 'b', 'blockquote', 'code', 'em','i','li',
-                        'ol', 'pre', 'strong', 'ul', 'h1', 'h2','h3', 'p']
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                            'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                            'h1', 'h2', 'h3', 'p']
         target.body_html = bleach.linkify(bleach.clean(
-            markdown(value, output_format='html'), tags=allowed_tags, strip=True))
+                markdown(value, output_format='html'),
+                tags=allowed_tags, strip=True))
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
-
