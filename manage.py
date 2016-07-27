f56d6417bffc34e402bbf16d0b60aed16ac4aad4
zhushengwei <15918660832@139.com>
2016-07-27 11:42:02 +0800

    test_five

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 "'启动脚本'"
 import os
 from app import create_app, db
-from app.models import User, Role, Permission, Post
+from app.models import User, Role, Permission, Post, Follow
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -12,12 +12,10 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
-
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post)
+    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role, Permission=Permission, Post=Post)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-
 
 @manager.command
 def test():
